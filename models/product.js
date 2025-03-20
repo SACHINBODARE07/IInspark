fdcf2650fbec55434a8baff1283a7b0bc82db811
Sachin Bodare <sachinbodare2@gmail.com>
2025-03-21 03:13:09 +0530

    Fixed Product model import issue

--- models/product.js
+++ models/product.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: { type: String, required: true },
-  gender: { type: String },
-  imageUrl: { type: String, required: true },
-}, { timestamps: true });
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    gender: { type: String },
+    imageUrl: { type: String, required: true }
+});
 
-// Ensure the model is defined only once
-module.exports = mongoose.models.Product || mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Product', ProductSchema);
+
+// module.exports = mongoose.models.Product || mongoose.model('Product', ProductSchema);
